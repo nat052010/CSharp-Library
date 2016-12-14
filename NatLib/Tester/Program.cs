ee6b82e814c7875b27f82930c434b3ad37881fdd
nat052010 <jonathanblumapas@gmail.com>
2016-12-14 10:24:32 +0800

    Add Debug Log

--- NatLib/Tester/Program.cs
+++ NatLib/Tester/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NatLib.DB;
+using NatLib.Debug;
 
 namespace Tester
 {
@@ -7,10 +9,15 @@ namespace Tester
     {
         private static void Main(string[] args)
         {
-/*
-            var sql = new MsSqlServer();
-            var test = sql.SqlExecCommand("pTest", new Dictionary<string, object> { {"test1", 5}, {"test2", true} });
-*/
+            var log = new Log();
+            log.Write("Error1");
+            log.Write("Error2");
+            Console.WriteLine("Writing Error");
+            Console.ReadLine();
+            /*
+                        var sql = new MsSqlServer();
+                        var test = sql.SqlExecCommand("pTest", new Dictionary<string, object> { {"test1", 5}, {"test2", true} });
+            */
         }
     }
 }
